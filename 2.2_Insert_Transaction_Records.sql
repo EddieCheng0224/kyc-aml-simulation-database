3fc51fad29dfc8c94f620c13bfab069e3aff4d01
EddieCheng0224 <eddiecheng777@gmail.com>
2025-03-29 14:38:00 -0700

    Update 2.2_Insert_Transaction_Records.sql

--- 2.2_Insert_Transaction_Records.sql
+++ 2.2_Insert_Transaction_Records.sql
@@ -1,5 +1,5 @@
 INSERT INTO transactions (user_id, amount, transaction_type, currency, recipient_account)
 VALUES
-    (1, 5000.00, 'deposit', 'USD', '9876543210'),
-    (1, 10000.00, 'withdrawal', 'USD', '1234567890'),
-    (2, 2000.00, 'deposit', 'GBP', '2345678901');
+    ((SELECT user_id FROM users WHERE id_number = 'A123456789'), 5000.00, 'deposit', 'USD', '9876543210'),
+    ((SELECT user_id FROM users WHERE id_number = 'A123456789'), 10000.00, 'withdrawal', 'USD', '1234567890'),
+    ((SELECT user_id FROM users WHERE id_number = 'B987654321'), 2000.00, 'deposit', 'GBP', '2345678901');
